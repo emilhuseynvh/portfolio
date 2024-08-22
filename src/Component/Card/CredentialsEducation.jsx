2fcfc7370151c559b3cd4983bc12fafb17b7a23f
emilhuseynvh <emilhuseynvh@gmail.com>
2024-08-22 18:46:16 +0400

    vercel.json added

--- src/Component/Card/CredentialsEducation.jsx
+++ src/Component/Card/CredentialsEducation.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { education } from '../../data/education';
+import Aos from 'aos';
 
 
 const CredentialsEducation = () => {
+    useEffect(() => {
+        Aos.init({
+            duration: 1500,
+            easing: 'ease',
+            once: true,
+        });
+    }, []);
     return (
         <div>
             <h4 className='mb-[37px] text-white text-[18px]'>EDUCATION</h4>
             {education.map((item, i) => {
                 return (
-                    <div className='pb-[110px]'>
+                    <div data-aos="zoom-in" className='pb-[110px]'>
                         <p className='text-[#BCBCBC] opacity-60 text-base font-medium mb-[8px]'>{item.year}</p>
                         <h4 className='text-lg text-[#5B78F6]'>{item.name}</h4>
                         <h6 className='text-sm text-[#BCBCBC] font-normal opacity-60 mb-[8px]'>{item.company}</h6>
--- src/Component/Card/CredentialsExperience.jsx
+++ src/Component/Card/CredentialsExperience.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { experince } from '../../data/experience'
+import Aos from 'aos';
 console.log(experince);
 
 
 const CredentialsExperience = () => {
+    useEffect(() => {
+        Aos.init({
+            duration: 1500,
+            easing: 'ease',
+            once: true,
+        });
+    }, []);
     return (
         <div>
             <h4 className='mb-[37px] text-white text-[18px]'>EXPERIENCE</h4>
             {experince.map((item, i) => {
                 return (
-                    <div className='pb-[110px]'>
+                    <div data-aos="zoom-in" className='pb-[110px]'>
                         <p className='text-[#BCBCBC] opacity-60 text-base font-medium mb-[8px]'>{item.year}</p>
                         <h4 className='text-lg text-[#5B78F6]'>{item.name}</h4>
                         <h6 className='text-sm text-[#BCBCBC] font-normal opacity-60 mb-[8px]'>{item.company}</h6>
