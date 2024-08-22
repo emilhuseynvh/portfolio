511fdd7f83fad13215360ce90d256f11f9c54ea5
emilhuseynvh <emilhuseynvh@gmail.com>
2024-08-22 18:50:36 +0400

    zoom-in

--- src/pages/Credentials.jsx
+++ src/pages/Credentials.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import CredentialsCard from '../Component/Card/CredentialsCard'
 import './../css/credential.css'
 import CredentialsExperience from '../Component/Card/CredentialsExperience'
 import CredentialsEducation from '../Component/Card/CredentialsEducation'
+import Aos from 'aos'
 
 const Credentials = () => {
+    useEffect(() => {
+        Aos.init({
+            duration: 1500,
+            easing: 'ease',
+            once: true,
+        });
+    }, []);
     return (
         <div className='container mx-auto flex gap-20'>
             <div className='w-[370px] h-[500px] sticky top-4'>
                 <CredentialsCard />
             </div>
             <div>
-                <div className='pb-[110px]'>
+                <div data-aos="zoom-in" className='pb-[110px]'>
                     <h4 className='mb-[37px] text-white text-[18px] '>ABOUT ME</h4>
                     <p className='text-base opacity-80 text-white '>As a Software Developer, I specialize in creating and maintaining high-quality software applications. My expertise spans across various programming languages, frameworks, and tools, enabling me to build robust and scalable solutions tailored to meet specific business needs. I am proficient in both front-end and back-end development, ensuring seamless integration and performance. My approach to development is grounded in problem-solving, continuous learning, and a keen eye for detail, which allows me to deliver efficient and innovative software solutions that drive user satisfaction and business growth.</p>
                 </div>
